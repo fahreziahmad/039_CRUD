67f97587f6abba40cb10c89ddf436a4aa069b501
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-10-20 13:01:40 +0700

    add start nodemon

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json);
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+})
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',       
+    password: 'panggunggembira623',   
+    database: 'biodata',
+    port: 3306
+})
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            console.error('Error fetching users:', err);
+            req.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    })
+});
+
+app.post('/api/user', (req, res) => {
+    const { nama, nim, kelas } = req.body;
+
+    if (!nama || !nim || !kelas) {
+        return res.status(400).json({ error: 'tolong isi nama, nim, dan kelas' });
+    }
+
+    db,query(
+        'INSERT INTO users (nama, nim, kelas) VALUES (?, ?, ?)',
+        [nama, nim, kelas],
+        (err, results) => {
+            if (err) {
+                console.error('Error adding user:', err);
+                return res.status(500).json({ error: 'Error adding user' });
+            }
+            res.status(201).json({ message: 'User added successfully'});
+        }
+    );
+});
+
+app.put('/api/user/:id', (req, res) => {
+    const userId = req.params.id;
+    const { nama, nim, kelas } = req.body;
+    db.query(
+        'update users set nama = ?, nim = ?, kelas = ? where id = ?',
+        [nama, nim, kelas, userId],
+        (err, results) => { 
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database error' });
+            }
+            res.json({ message: 'User updated successfully' });
+        }
+    )
+})
+
+app.delete('/api/user/:id', (req, res) => {
+    const userId = req.params.id;
+    db.query('DELETE FROM mahasiswa WHERE id = ?', [userId], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({message: 'Database erorr'});
+        }
+        res.json({ message: 'User deleted successfully' });
+    })
+})
+
